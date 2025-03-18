5f3abb425518992f1bd7ca6327fbea1a0a95d7fb
Gisela Blackstadius <gisela.blackstadius@accenture.com>
2025-03-18 13:41:09 +0100

    Add glowing heading

--- src/app/components/HomePageContent/HomePageContent.tsx
+++ src/app/components/HomePageContent/HomePageContent.tsx
@@ -3,9 +3,7 @@ import { motion, useAnimate } from 'motion/react';
 import SearchForm from '../LyricsForm/SearchForm';
 import styles from './HomePageContent.module.css';
 import { useEffect } from 'react';
-import Lyrics from '../Lyrics/Lyrics';
-
-Lyrics;
+import Hi from '../../../../public/Hi!.svg';
 
 const sequence: any[] = [
   ['#hiHeading', { scale: [0.4, 1] }, { ease: 'circInOut', duration: 1 }],
@@ -25,25 +23,21 @@ const sequence: any[] = [
 
 const MainContent = () => {
   const [scope, animate] = useAnimate();
-
+  console.log(Hi);
   useEffect(() => {
     animate(sequence);
   }, []);
 
   return (
-    <div className={styles.blurredBackground}>
-      <div ref={scope} className={styles.background}>
-        <motion.div id="hiHeading">
-          <h1 className={styles.headingOne}>Hi!</h1>
+    <div ref={scope} className={styles.background}>
+      <Hi className={styles.headingOne} id="hiHeading" />
+      <div className={styles.content}>
+        <motion.div id="searchHeading">
+          <h1 className={styles.headingThree}>
+            Search for the lyrics you want to understand
+          </h1>
         </motion.div>
-        <div className={styles.content}>
-          <motion.div id="searchHeading">
-            <h1 className={styles.headingThree}>
-              Search for the lyrics you want to understand
-            </h1>
-          </motion.div>
-          <SearchForm />
-        </div>
+        <SearchForm />
       </div>
     </div>
   );
